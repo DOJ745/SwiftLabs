296b5b35b8a931ae8fede93fd0291c5921d1c308
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-13 08:57:59 +0300

    Added app for LB7

---
+++ LB7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LB7
+//
+//  Created by AdminUser on 8.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello LB7")
+            .font(.title)
+            .fontWeight(.heavy)
+            .foregroundColor(.red)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LB7/LB7App.swift
@@ -0,0 +1,17 @@
+//
+//  LB7App.swift
+//  LB7
+//
+//  Created by AdminUser on 8.04.21.
+//
+
+import SwiftUI
+
+@main
+struct LB7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
